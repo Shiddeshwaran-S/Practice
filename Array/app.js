35720e1d9c58e7a76666770ff0fc495a9fa61f04
Shiddeshwaran-Selvarasu <20p244@kce.ac.in>
2023-03-21 17:11:38 +0530

    arrays

--- Array/app.js
+++ Array/app.js
@@ -2,3 +2,23 @@ var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(arr['2']);
 console.log(arr['02']);
+
+/*****************************************************************************
+var even = arr.group((ele)=>{
+    return ele%2 == 0;
+});
+
+var odd = arr.group((ele)=>{
+    return ele%2 != 0;
+});
+    group is available in array object only on safari browser
+*****************************************************************************/
+
+console.log(even.true);
+
+var arr2 = [...arr];
+
+console.log(arr.pop());
+console.log(arr);
+
+console.log(arr2);
