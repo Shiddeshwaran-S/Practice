b37c296ce5ae1cfdaeb41c41d60ea1b02fdb02e3
Shiddeshwaran-Selvarasu <20p244@kce.ac.in>
2023-03-23 14:58:22 +0530

    unit testing

---
+++ Testing/test.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function (done) {
+      expect([1, 2, 3].indexOf(4)).to.equal(-1);
+      done();
+    });
+
+    it('should return value when the value is present', function (done) {
+      expect([1, 2, 3].indexOf(2)).to.equal([1, 2, 3].indexOf(2));
+      done();
+    });
+  });
+});
