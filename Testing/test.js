37127faee91e55ff8a36bc59def96c3c2ef4c538
Shiddeshwaran-Selvarasu <20p244@kce.ac.in>
2023-03-23 16:56:57 +0530

    suite

--- Testing/test.js
+++ Testing/test.js
@@ -1,13 +1,14 @@
 const expect = require('chai').expect;
+const { suite, describe, it } = require('mocha');
 
-describe('Array', function () {
+suite('Array', function () {
   describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function (done) {
+    it.only('should return -1 when the value is not present', function (done) {
       expect([1, 2, 3].indexOf(4)).to.equal(-1);
       done();
     });
 
-    it('should return value when the value is present', function (done) {
+    it.skip('should return value when the value is present', function (done) {
       expect([1, 2, 3].indexOf(2)).to.equal(1);
       done();
     });
