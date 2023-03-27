8909280624e52d8aa1e0d5f26633f9d3a3bb74e5
Shiddeshwaran-Selvarasu <20p244@kce.ac.in>
2023-03-27 17:31:48 +0530

    final working format

--- MQTT/index.js
+++ MQTT/index.js
@@ -1,10 +1,50 @@
 const mqttprotocol = require('aws-iot-device-sdk');
 
-const device = awsIot.device({
-    clientId: 'RasperryMQTTClient',
-    host: 'a2q5k4pqd9e0uy-ats.iot.us-east-1.amazonaws.com',
+const device = mqttprotocol.device({
+    clientId: 'DemoPC',
+    host: 'af45gc5tdv54r-ats.iot.eu-north-1.amazonaws.com',
     port: 8883,
-    keyPath: './AWS_Rasperry_secrets/private.pem.key',
-    certPath: './AWS_Rasperry_secrets/certificate.pem.crt',
-    caPath: './AWS_Rasperry_secrets/AmazonRootCA1.pem',
+    keyPath: './awskeys/AWS-private.pem.key',
+    certPath: './awskeys/AWS-certificate.pem.crt',
+    caPath: './awskeys/AmazonRootCA1.pem',
 });
+
+device.on('message', (topic, payload) => {
+    console.log('message', topic, payload.toString());
+});
+
+
+
+device.on('connect', () => {
+    console.log('Connected to AWS IoT');
+    device.subscribe('$aws/things/DemoPC/shadow/update/accepted');
+    device.subscribe('$aws/things/DemoPC/shadow/get/accepted');
+    device.subscribe('$aws/things/DemoPC/shadow/get/rejected');
+    device.subscribe('$aws/things/DemoPC/shadow/update/rejected');
+    device.subscribe('$aws/things/DemoPC/shadow/update/delta');
+    device.subscribe('$aws/things/DemoPC/shadow/update/documents');
+    device.subscribe('$aws/things/DemoPC/shadow/delete/accepted');
+    device.subscribe('$aws/things/DemoPC/shadow/delete/rejected');
+    device.subscribe('$aws/things/DemoPC/shadow/delete/documents');
+});
+
+device.publish('$aws/things/DemoPC/shadow/get', JSON.stringify({
+    "state": { 
+        "desired": { 
+            "welcome": "aws-iot", 
+            "1": "One", 
+            "2": "Two", 
+            "numeral": 1 
+        }, 
+        "reported": { 
+            "welcome": "aws-iot", 
+            "1": "Once", 
+            "2": "Twice" 
+        }, 
+        "delta": { 
+            "1": "One", 
+            "2": "Two", 
+            "numeral": 1 
+        }, 
+    },
+}));
